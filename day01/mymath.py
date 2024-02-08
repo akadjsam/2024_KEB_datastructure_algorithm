1a535b2acc94738e483ab070c89bcde1bfc49326
akadjsam <akadjsam@gmail.com>
2024-02-08 11:02:46 +0900

    v0.5 combinationreview day20

--- day01/mymath.py
+++ day01/mymath.py
@@ -1,9 +1,16 @@
-def factorial(n) -> int: #
-    if n <= 1:
-        return 1
-    else:
-        return n * factorial(n-1) #파이썬의 경우는 재귀함수를 많이 호출하게 되면 자동으로 에러를 발생시킨다.(1000번까지 발생)
-    #재귀함수는 스택메모리 사용
+import time
+
+def factorial(n) -> int:
+    result = 1
+    for i in range(1,n+1):
+        result *= i
+    return result
+    # if n <= 1:
+    #     return 1
+    # else:
+    #     return n * factorial(n-1) #파이썬의 경우는 재귀함수를 많이 호출하게 되면 자동으로 에러를 발생시킨다.(1000번까지 발생)
+    # # 재귀함수는 스택메모리 사용
+
 def nCr(n,r) -> int:
     """
     조합 함수
@@ -11,6 +18,9 @@ def nCr(n,r) -> int:
     :param r:
     :return : int:
     """
+    start = time.time()
     numerator = factorial(n)
     denominator = factorial(n-r) * factorial(r)
+    end = time.time()
+    print(end - start) #단일책임의 원칙에 벗어남. 한 함수에 2가지의 역할을 하고 있음.
     return int(numerator / denominator)
