d096c44ed6af6545df883f832f8d3ab68c08aaef
akadjsam <akadjsam@gmail.com>
2024-02-08 10:41:30 +0900

    v0.3 recursion preview day20

--- day01/day20.py
+++ day01/day20.py
@@ -12,10 +12,15 @@
 # print(nCr(7,0))
 
 def factorial(n) -> int: #
-    result = 1
-    for i in range(1,n+1):
-        result *= i
-    return result
+    # result = 1
+    # for i in range(1,n+1):
+    #     result *= i
+    # return result
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n-1) #파이썬의 경우는 재귀함수를 많이 호출하게 되면 자동으로 에러를 발생시킨다.(1000번까지 발생)
+
 def nCr(n,r) -> int:
     """
     조합 함수
