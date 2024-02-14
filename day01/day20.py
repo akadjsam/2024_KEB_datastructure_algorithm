ced855588ad7766b480805f93559884101d147c6
akadjsam <akadjsam@gmail.com>
2024-02-14 16:08:45 +0900

    v1.7 recursion

--- day01/day20.py
+++ day01/day20.py
@@ -1,28 +1,13 @@
-graph =[
-    [0,1,1,0,0,0],
-    [1,0,0,1,0,0],
-    [1,0,0,1,0,0],
-    [0,1,1,0,1,1],
-    [0,0,0,1,0,1],
-    [0,0,0,1,1,0]
-]
-name_array=['문별','솔라','휘인','쯔위','선미','화사']
+def decimal_to_octal(number:int) -> str:
+    """
+    decimal -> octal
+    :param number: int
+    :return: str
+    """
+    octal = ""
+    while number > 0:
+        octal = str(number % 8) + octal
+        number = number // 8
+    return octal
 
-def dfs(g, v, visited):
-    visited[v] = True
-    print(name_array[v],end=' ')
-    for i in range(len(g)):
-        if g[v][i] == True and not visited[i]:
-            dfs(g,i,visited)
-
-visited = [False]*len(graph)
-
-dfs(graph,0,visited)
-'''
-G1 = Graph(5)
-G1.graph[0][2] = 1; G1.graph[0][3] = 1
-G1.graph[1][2] = 1
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
-G1.graph[3][0] = 1; G1.graph[3][2] = 1
-G1.graph[2][4] = 1; G1.graph[4][2] = 1
-'''
+print(decimal_to_octal(10))
