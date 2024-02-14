72dd480ba53cbfcad0e547f8b5798ec8a472fb06
akadjsam <akadjsam@gmail.com>
2024-02-14 17:30:34 +0900

    v2.0 fibo memoization

--- day01/day20.py
+++ day01/day20.py
@@ -17,8 +17,29 @@ def fibo_repetition(number : int) -> int:
     for _ in range(number):
         a,b = b, a+b
     return a
-for i in range(0, 20):
+
+memo = [None for _ in range(100)]
+def fibo_memoization(number : int, memo) -> int:
+    """
+    fibonacci function by recursion with memoization
+    :param n: int
+    :return: int
+    """
+    if memo[number] is not None:
+        return memo[number]
+    if number < 2:
+        result = number
+    else:
+        result = fibo_memoization(number-1,memo) + fibo_memoization(number-2,memo)
+    memo[number] = result
+    return result
+
+
+for i in range(0, 40):
+    print(fibo_memoization(i,memo), end=' ')
+print()
+for i in range(0, 40):
     print(fibo_recursion(i), end=' ')
 print()
-for i in range(0, 20):
+for i in range(0, 40):
     print(fibo_repetition(i), end=' ')
