a1d3bc546c45c1008c6679a09de01b990360f919
akadjsam <akadjsam@gmail.com>
2024-02-16 11:19:58 +0900

    v2.5 dfs bfs

--- day01/day20.py
+++ day01/day20.py
@@ -1,24 +1,36 @@
-def bianry_search(arr, data):
-    start = 0
-    end = len(arr)-1
-    while(start<=end):
-        n = (start+end)//2
-        if data == arr[n]:
-            return n
-        elif data > arr[n]:
-            start = n+1
-        else:
-            end = n-1
-    return -1
-def search(arr, data):
-    for i in range(len(arr)):
-        print(arr[i], end=' ')
-        if data == arr[i]:
-            print()
-            return i
-    return -1
+from collections import deque
 
-a = [1,2,3,4,5,6]
-find_data = 4
-print(search(a,find_data))
-print(bianry_search(a,find_data))
+graph =[
+    [0,1,1,0,0,0],
+    [1,0,0,1,0,0],
+    [1,0,0,1,0,0],
+    [0,1,1,0,1,1],
+    [0,0,0,1,0,1],
+    [0,0,0,1,1,0]
+]
+
+def dfs(g, v, visited):
+    visited[v] = True
+    print(chr(ord('A')+v),end=' ')
+    for i in range(len(g)):
+        if g[v][i] == True and not visited[i]:
+            dfs(g,i,visited)
+
+visited = [False]*len(graph)
+
+dfs(graph,0,visited)
+print()
+
+def bfs(g,v,visited):
+    queue = deque([v])
+    visited[v] = 1
+    while queue:
+        v = queue.popleft()
+        print(chr(ord('A')+v),end=' ')
+        for i in range(len(g)):
+            if g[v][i] == True and not visited[i]:
+                queue.append(i)
+                visited[i] = 1
+
+visited = [False]*len(graph)
+bfs(graph,0,visited)
